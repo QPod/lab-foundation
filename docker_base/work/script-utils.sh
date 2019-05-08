954141ac75a0446569b44a14b8d29a1bd79b0f69
Bibo Hao <haobibo@users.noreply.github.com>
2019-05-09 02:50:03 +0800

    debug: add octave

--- docker_base/work/script-utils.sh
+++ docker_base/work/script-utils.sh
@@ -1,6 +1,6 @@
 
 # function to debug, resolve package names from a text file and display.
-install_echo() { cat $1 | cut -d "%" -f 1 | sed '/^$/d' | xargs -n1 echo "#" ; }
+install_echo() { cat $1 | cut -d "%" -f 1 | sed '/^$/d' | xargs -n1 printf '%s\n' ; }
 
 # function to install apt-get packages from a text file which lists package names (add comments with % char)
 install_apt()  { apt-get -y update --fix-missing && apt-get -qq install -y --no-install-recommends `cat $1 | cut -d '%' -f 1` ; }
@@ -14,17 +14,23 @@ install_pip()  { cat $1 | cut -d "%" -f 1 | sed '/^$/d' | xargs -n1 pip install 
 # function to install R packages from a text file which lists package names (add comments with % char, use quiet=T to be less verbose)
 install_R()    { R -e "options(Ncpus=4);lapply(scan('$1','c',comment.char='%'),function(x){cat(x,system.time(install.packages(x,clean=T,quiet=T)),'\n')})"; }
 
-# function to install python packages with go from a text file which lists package names (add comments with % char)
+# function to install go packages with go from a text file which lists package names (add comments with % char)
 install_go()   { cat $1 | cut -d "%" -f 1 | sed '/^$/d' | xargs -n1 go get -u ; }
 
 # function to install julia packages from a text file which lists package names (add comments with % char)
 install_julia()  { julia -e "import Pkg; l=filter(x->length(x)>0, [split(i,r\"%| |\t\")[1] for i in readlines(\"$1\")]); Pkg.add(l)" ; }
 
+# function to install octave packages with go from a text file which lists package names (add comments with % char)
+install_octave() { cat $1 | cut -d "%" -f 1 | sed '/^$/d' | xargs -n1  -I {} sh -c 'octave --eval "pkg install -forge {}"' ; }
+
 # function to download a ZIP file with wget and unzip it to /opt/
 install_zip()  { wget -nv $1 -O /tmp/TMP.zip && unzip -q /tmp/TMP.zip -d /opt/ && rm /tmp/TMP.zip ; }
 
-# function to download a .tar.gz file with wget and unzip it to /opt/
+# function to download a .tar.gz file with wget and unzip it to /opt/, add a second argument to extract only those file
 install_tar_gz()  { wget -nv $1 -O /tmp/TMP.tar.gz && tar -C /opt/ -xzf /tmp/TMP.tar.gz $2 && rm /tmp/TMP.tar.gz ; }
+
+# function to download a .tar.xz file with wget and unzip it to /opt/, add a second argument to extract only those file
+install_tar_xz()  { wget -nv $1 -O /tmp/TMP.tar.xz && tar -C /opt/ -xJf /tmp/TMP.tar.xz $2 && rm /tmp/TMP.tar.xz ; }
 
 # function to install java packages from a text file which lists JAR file maven full names (add comments with % char)
 install_mvn()  { cat $1 | cut -d "%" -f 1 | xargs -n1 -I {} mvn dependency:copy -DlocalRepositoryDirectory="/tmp/m2repo" -Djavax.net.ssl.trustStorePassword=changeit -Dartifact="{}" -DoutputDirectory="$2" ; }
