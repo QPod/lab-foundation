ca463b4ec51a535f9bb0ea460e71959835d76a51
Bibo Hao <haobibo@users.noreply.github.com>
2020-08-13 09:42:28 +0800

    dbg

--- docker_base/work/script-setup.sh
+++ docker_base/work/script-setup.sh
@@ -55,7 +55,7 @@ setup_node() {
 setup_R_base() {
        apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 \
     && echo "deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" > /etc/apt/sources.list.d/cran.list \
-    && install_apt  ./install_list_R.apt \
+    && install_apt  /opt/utils/install_list_R.apt \
     && echo "@ Version of R:" && R -e "R.version.string;"  \
     && ( type java && type R && R CMD javareconf || true ) \
     && echo "options(repos=structure(c(CRAN=\"https://cloud.r-project.org\")))" >> /etc/R/Rprofile.site \
