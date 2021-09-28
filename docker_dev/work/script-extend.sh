4f00ecaf4e9aed4c4fc57723133a0605553adcdb
Bibo Hao <haobibo@users.noreply.github.com>
2021-09-29 01:56:43 +0800

    bugfix: code-server install

--- docker_dev/work/script-extend.sh
+++ docker_dev/work/script-extend.sh
@@ -81,7 +81,7 @@ setup_jupyter_extensions() {
 
 setup_vscode_base() {
      VERSION_CODER=$(curl -sL https://github.com/cdr/code-server/releases.atom | grep "releases/tag" | head -1 | grep -Po '(\d[\d|.]+)') \
-  && install_tar_gz "https://github.com/cdr/code-server/releases/download/v${VERSION_CODER}/code-server-${VERSION_CODER}-linux-x86_64.tar.gz" \
+  && install_tar_gz "https://github.com/cdr/code-server/releases/download/v${VERSION_CODER}/code-server-${VERSION_CODER}-linux-amd64.tar.gz" \
   && mv /opt/code-server* /opt/code-server \
   && ln -s /opt/code-server/bin/code-server /usr/bin/ \
   && printf "#!/bin/bash\n/opt/code-server/bin/code-server --port=8888 --auth=none --disable-telemetry $HOME\n" > /usr/local/bin/start-code-server.sh \
