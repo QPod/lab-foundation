1f79c9479787592f47e30529f3188c4e28f38d69
Bibo Hao <haobibo@users.noreply.github.com>
2024-02-09 20:22:25 +0000

    debug pkg version

--- docker_atom/work/script-setup.sh
+++ docker_atom/work/script-setup.sh
@@ -136,14 +136,15 @@ setup_node() {
   # NODEJS_VERSION_MAJOR="v14" && grep "v${NODEJS_VERSION_MAJOR}."
      ARCH="x64" \
   && NODEJS_VERSION=$(curl -sL https://github.com/nodejs/node/releases.atom | grep 'releases/tag' | head -1 | grep -Po '\d[.\d]+') \
-  && NODEJS_VERSION_MAJOR=$(echo "${NODEJS_VERSION}" | cut -d '.' -f1 ) \
-  && install_tar_gz "https://nodejs.org/download/release/latest-v${NODEJS_VERSION_MAJOR}.x/node-v${NODEJS_VERSION}-linux-${ARCH}.tar.gz" \
+  && NODEJS_URL="https://nodejs.org/download/release/latest-v${NODEJS_VERSION_MAJOR}.x/node-v${NODEJS_VERSION}-linux-${ARCH}.tar.gz" \
+  && echo "Downloading NodeJS from: ${NODEJS_URL}" \
+  && install_tar_gz ${NODEJS_URL} \
   && mv /opt/node* /opt/node \
   && echo  "PATH=/opt/node/bin:$PATH" >> /etc/bash.bashrc \
   && export PATH=/opt/node/bin:$PATH \
   && npm install -g npm \
-  && corepack enable && yarn set version stable && yarn install \
-  && echo "@ Version of Node, npm, and yarn: $(node -v) $(npm -v)" \
+  && corepack enable && yarn set version stable \
+  && echo "@ Version of Node and npm: $(node -v) $(npm -v)" \
   && echo "@ Version of Yarn: $(yarn -v)"
 }
 
