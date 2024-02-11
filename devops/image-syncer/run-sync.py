864d641e1d8a6dd7ac3563baf4ab3cec154036b1
Bibo Hao <haobibo@users.noreply.github.com>
2024-02-11 03:54:36 +0000

    modified:   devops/image-syncer/run-sync.py

--- devops/image-syncer/run-sync.py
+++ devops/image-syncer/run-sync.py
@@ -5,11 +5,8 @@ import tempfile
 import subprocess
 
 
-def generate(image, tag=None):
-    # , 'cn-shanghai', 'cn-shenzhen', 'cn-chengdu', 'cn-hongkong', 'us-west-1', eu-central-1
-    destinations = ['cn-beijing', 'cn-hangzhou']
-    destinations = ['registry.%s.aliyuncs.com' % i for i in destinations]
-
+def generate(image: str, target_registries: list, tag: list=None):
+    """Generate a config item which will be used by `image-syncer`."""
     crend_uname = os.environ.get('DOCKER_MIRROR_REGISTRY_USERNAME', None)
     crend_paswd = os.environ.get('DOCKER_MIRROR_REGISTRY_PASSWORD', None)
 
@@ -17,7 +14,7 @@ def generate(image, tag=None):
         print('ENV variable requried: DOCKER_MIRROR_REGISTRY_USERNAME and DOCKER_MIRROR_REGISTRY_PASSWORD !')
         sys.exit(-2)
 
-    for dest in destinations:
+    for dest in target_registries:
         src = "%s:%s" % (image, tag) if tag is not None else image
 
         yield {
@@ -29,28 +26,38 @@ def generate(image, tag=None):
             }
         }
 
+
+def sync_image(cfg: dict):
+    """Run the sync task using `image-syncer` with given config item."""
+    with tempfile.NamedTemporaryFile(mode='wt', encoding='UTF-8', suffix='.json') as fp:
+        json.dump(cfg, fp, ensure_ascii=False, indent=2, sort_keys=True)
+        fp.flush()
+        ret = 0
+        try:
+            subprocess.run(['image-syncer', '--proc=8', '--retries=2', '--config=' + fp.name], check=True)
+        except subprocess.CalledProcessError as e:
+            ret = e.returncode
+            print(e)
+    return ret
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
-    segs = args[1:]
+    img, segs = args[0], args[1:]
 
     if len(args) < 1 or len(segs) > 2:
         print('Usage:')
         print('\tDOCKER_MIRROR_REGISTRY_USERNAME="*" DOCKER_MIRROR_REGISTRY_PASSWORD="*" python run-sync.py repo/image:tag')
         sys.exit(-1)
 
-    img = args[0]
     tag = None if len(segs) == 0 else ','.join(segs[1:])
 
-    configs = generate(image=img, tag=tag)
+    # , 'cn-shanghai', 'cn-shenzhen', 'cn-chengdu', 'cn-hongkong', 'us-west-1', eu-central-1
+    destinations = ['cn-beijing', 'cn-hangzhou']
+    destinations = ['registry.%s.aliyuncs.com' % i for i in destinations]
+
+    configs = generate(image=img, tag=tag, target_registries=destinations)
     for i, c in enumerate(configs):
-        with tempfile.NamedTemporaryFile(mode='wt', encoding='UTF-8', suffix='.json') as fp:
-            json.dump(c, fp, ensure_ascii=False, indent=2, sort_keys=True)
-            fp.flush()
-            ret = 0
-            try:
-                subprocess.run(['image-syncer', '--proc=8', '--retries=2', '--config=' + fp.name], check=True)
-            except subprocess.CalledProcessError as e:
-                ret = e.returncode
-                print(e)
-    
+        ret = sync_image(cfg=c)
+
     sys.exit(ret)
