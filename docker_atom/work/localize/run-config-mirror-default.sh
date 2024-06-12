0aa33069cf43beaad97521547c722202461c6544
Bibo Hao <haobibo@users.noreply.github.com>
2024-06-12 14:04:34 +0800

    Add localize (#99)
    * add localize
    * update localize script
    * add localize profile

---
+++ docker_atom/work/localize/run-config-mirror-default.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+set -ex
+
+export TZ=${TZ:="Asia/Shanghai"}
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ >/etc/timezone
+echo "Setup timezone, current date: $(date)"
+
+if [ -f /etc/apt/sources.list ]; then
+  echo "Found Ubuntu/debian system, using default ubuntu/debian mirror"
+fi
+
+if [ -f "$(which python)" ]; then
+  echo "Found python, using default pypi source in /etc/pip.conf"
+  pip config list
+fi
+
+if [ -f "$(which npm)" ]; then
+  echo "Found npm, using default npm mirror"
+  npm config list
+fi
+
+if [ -f "$(which go)" ]; then
+  echo "Found golang, getting GO env:"
+  go env | sort
+fi
---
+++ docker_atom/work/localize/run-config-mirror-tencent-pub.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+set -ex
+
+echo "Setup mirror config for Tencent Cloud VPC Environment..."
+
+export TZ=${TZ:="Asia/Shanghai"}
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ >/etc/timezone
+echo "Setup timezone, current date: $(date)"
+
+if [ -f /etc/apt/sources.list ]; then
+  echo "Found Ubuntu/debian system, setting ubuntu/debian mirror"
+
+  sed -i 's/mirrors.*.com\/ubuntu/mirrors.tencent.com\/ubuntu/' /etc/apt/sources.list
+  sed -i 's/archive.ubuntu.com\/ubuntu/mirrors.tencent.com\/ubuntu/' /etc/apt/sources.list
+  sed -i 's/security.ubuntu.com\/ubuntu/mirrors.tencent.com\/ubuntu/' /etc/apt/sources.list
+
+  sed -i 's/deb.debian.org\/debian/mirrors.tencent.com\/debian/' /etc/apt/sources.list
+fi
+
+if [ -f "$(which python)" ]; then
+  echo "Found python, setting pypi source in /etc/pip.conf"
+  cat >/etc/pip.conf <<EOF
+[global]
+progress_bar=off
+root-user-action=ignore
+retries=5
+timeout=180
+trusted-host=pypi.python.org pypi.org files.pythonhosted.org mirrors.tencent.com
+index-url=http://mirrors.tencent.com/pypi/simple/
+EOF
+  pip config list
+fi
+
+if [ -f "$(which npm)" ]; then
+  echo "Found npm, setting npm mirror"
+  npm config set registry https://registry.npmmirror.com
+  npm config list
+fi
+
+if [ -f "$(which go)" ]; then
+  echo "Found golang, setting GOPROXY"
+  export GOPROXY=https://mirrors.tencent.com/go/
+  go env | grep 'PROXY'
+fi
