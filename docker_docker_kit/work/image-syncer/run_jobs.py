a29bf89c946ca362dab94f76281a6ab902812b71
Bibo Hao <haobibo@users.noreply.github.com>
2025-04-06 15:47:15 +0000

    update image sync

--- docker_docker_kit/work/image-syncer/run_jobs.py
+++ docker_docker_kit/work/image-syncer/run_jobs.py
@@ -1,3 +1,4 @@
+import argparse
 import multiprocessing
 import os
 import sys
@@ -9,6 +10,7 @@ import run_sync
 
 def get_job_names_from_yaml(file_path):
     """Get all job names from GitHub Actions config file"""
+    images = []
     with open(file_path, 'r') as file:
         try:
             yaml_content = yaml.safe_load(file)
@@ -17,10 +19,10 @@ def get_job_names_from_yaml(file_path):
             for _, v in jobs.items():
                 name = v.get('name')
                 if name is not None:
-                    yield name
+                    images.extend(name.split(','))
         except yaml.YAMLError as exc:
             print(f"Error parsing YAML file: {exc}")
-            return []
+    return images
 
 
 def main():
@@ -28,17 +30,19 @@ def main():
     registry_url_src = os.environ.get("REGISTRY_URL", 'docker.io')
     registry_url_dst = os.environ.get("DOCKER_MIRROR_REGISTRY", None)
 
-    images = []
-    job_names = get_job_names_from_yaml('.github/workflows/build-docker.yml')
-    for name in job_names:
-        images.extend(name.split(','))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--workflow-file', type=str, default='.github/workflows/build-docker.yml', help='GitHub actions workflow file')
+    parser.add_argument('--image-namespace', type=str, default=img_namespace, help='namespace of the image')
+    parser.add_argument('--source-registry', type=str, default=registry_url_src, help='REGISTRY_URL')
+    parser.add_argument('--target-registry', type=str, default=registry_url_dst, action='extend', nargs='*', help='Target registry URL')
+    args = parser.parse_args()
 
     list_tasks = []
-    for image in images:
-        img = '/'.join([img_namespace, image])
+    for image in get_job_names_from_yaml(args.workflow_file):
+        img = '/'.join([args.image_namespace, image])
         print("Docker image sync job name found:", img)
         configs = run_sync.generate(
-            image=img, tags=None, source_registry=registry_url_src, target_registries=registry_url_dst
+            image=img, tags=None, source_registry=args.source_registry, target_registries=args.target_registry
         )
         for _, c in enumerate(configs):
             list_tasks.append(c)
