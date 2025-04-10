4720117e006092b348ef48c8c9c232d57a9b36f2
Bibo Hao <haobibo@users.noreply.github.com>
2025-04-10 14:29:59 +0800

    update localize scripts

--- docker_atom/work/localize/run-config-mirror-default.sh
+++ docker_atom/work/localize/run-config-mirror-default.sh
@@ -5,26 +5,26 @@ export TZ=${TZ:="Asia/Shanghai"}
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ >/etc/timezone
 echo "Setup timezone, current date: $(date)"
 
-if [ -f /etc/apt/sources.list ]; then
+if [ -f /etc/apt/sources.list ] ; then
   echo "Found Ubuntu/debian system, using default ubuntu/debian mirror"
 fi
 
-if command -v python &> /dev/null; then
+if [ -f "$(which python)" ] ; then
   echo "Found python, using default pypi source in /etc/pip.conf"
   pip config list
 fi
 
-if [ -f "$(which npm)" ]; then
+if [ -f "$(which npm)" ] ; then
   echo "Found npm, using default npm mirror"
   npm config list
 fi
 
-if command -v go &> /dev/null; then
+if [ -f "$(which go)" ] ; then
   echo "Found golang, getting GO env:"
   go env | sort
 fi
 
-if command -v R &> /dev/null; then
+if [ -f "$(which R)" ] ; then
   echo "Found R, getting CRAN mirror"
   R -e "options('repos');"
 fi
